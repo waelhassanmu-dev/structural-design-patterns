76112efdb2dee6968fd95f5fc9db896245b03dbf
waelhassanmu-dev <waelhassanmu-dev@gmail.com>
2023-09-01 15:37:29 +0400

    solving it by composite design pattern

---
+++ src/main/java/com/example/structural/composite/solution/Component.java
@@ -0,0 +1,6 @@
+package com.example.structural.composite.solution;
+
+public interface Component {
+
+    void render();
+}
---
+++ src/main/java/com/example/structural/composite/solution/Shape.java
@@ -0,0 +1,8 @@
+package com.example.structural.composite.solution;
+
+public class Shape implements Component {
+    @Override
+    public void render() {
+        System.out.println("render shape");
+    }
+}
