3ae7a7d4358dcaee75b42e51d9b639624616b603
waelhassanmu-dev <waelhassanmu-dev@gmail.com>
2023-09-01 14:59:05 +0400

    project init

---
+++ src/main/java/com/example/structural/StructuralRunner.java
@@ -0,0 +1,13 @@
+package com.example.structural;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StructuralRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StructuralRunner.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/structural/StructuralRunnerTests.java
@@ -0,0 +1,13 @@
+package com.example.structural;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StructuralRunnerTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
